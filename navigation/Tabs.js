b699673f4cd88207f570063d3d82902feaed099c
saduni-udeshika <saduniudeshika0@gmail.com>
2023-03-18 21:41:49 +0530

    Add bottom navigation

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,13 +1,16 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { StyleSheet, View, Text, Image } from "react-native"
+import Events from "../screens/Events"
 import Home from "../screens/Home"
 import Recipe from "../screens/Recipe"
+import Shops from "../screens/Shops"
+import Tips from "../screens/Tips"
 
 const Tab = createBottomTabNavigator()
 const Tabs = () => {
   return (
     <Tab.Navigator
-    tabBarOptions={{
+      tabBarOptions={{
         showLabel: false,
         style: {
           position: "absolute",
@@ -53,35 +56,125 @@ const Tabs = () => {
         }}
       />
       <Tab.Screen
-      name="Recipe"
-      component={Recipe}
-      options={{
-        tabBarIcon: ({ focused }) => (
-          <View
-            style={{
-              alignItems: "center",
-              justifyContent: "center",
-              top: 2,
-            }}
-          >
-            <Image
-            //   source={require("../assets/home-icon.jpg")}
-              resizeMode="contain"
+        name="Recipe"
+        component={Recipe}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
               style={{
-                width: 25,
-                height: 25,
-                tintColor: focused ? "#e32f45" : "#748c94",
+                alignItems: "center",
+                justifyContent: "center",
+                top: 2,
               }}
-            />
-            <Text
-              style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
             >
-              RECIPIES
-            </Text>
-          </View>
-        ),
-      }}
-    />
+              <Image
+                //   source={require("../assets/home-icon.jpg")}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? "#e32f45" : "#748c94",
+                }}
+              />
+              <Text
+                style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
+              >
+                RECIPIES
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Tips"
+        component={Tips}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                top: 2,
+              }}
+            >
+              <Image
+                //   source={require("../assets/home-icon.jpg")}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? "#e32f45" : "#748c94",
+                }}
+              />
+              <Text
+                style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
+              >
+                TIPS
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Shops"
+        component={Shops}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                top: 2,
+              }}
+            >
+              <Image
+                //   source={require("../assets/home-icon.jpg")}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? "#e32f45" : "#748c94",
+                }}
+              />
+              <Text
+                style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
+              >
+                SHOPS
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Events"
+        component={Events}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                top: 2,
+              }}
+            >
+              <Image
+                //   source={require("../assets/home-icon.jpg")}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? "#e32f45" : "#748c94",
+                }}
+              />
+              <Text
+                style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
+              >
+                EVENTS
+              </Text>
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
