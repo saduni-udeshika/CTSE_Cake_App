b437b77ade694bfc59a055822695346911a5d155
saduni-udeshika <saduniudeshika0@gmail.com>
2023-03-22 14:21:12 +0530

    Add background image

---
+++ screens/Home.js
@@ -0,0 +1,36 @@
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+
+const image = {};
+
+export class Home extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Inside</Text>
+      </ImageBackground>
+    </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+});
+
+export default Home
