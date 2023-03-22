f9b6dd71e5a7ebf1a016d699ddacc20a1be4f92c
saduni-udeshika <saduniudeshika0@gmail.com>
2023-03-22 09:01:48 +0530

    Add Recipes

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
 
-const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+export class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Home</Text>
+      </View>
+    )
+  }
 }
 
 export default Home
