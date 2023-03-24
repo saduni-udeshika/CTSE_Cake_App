425d868d3801d5bf1999372df78ef4457492dc26
Sasini Perera <sasiniperera24@gmail.com>
2023-03-25 01:24:19 +0530

    events CRUD operations

--- App.js
+++ App.js
@@ -33,7 +33,8 @@ import Tabs from "./navigation/Tabs";
 import Details from "./screens/Details";
 import TipsList from './screens/TipsList';
 import NewTip from './screens/NewTip';
-
+import EventsList from "./screens/EventsList";
+import NewEvent from "./screens/NewEvent";
 
 const Stack = createStackNavigator()
 
@@ -49,6 +50,9 @@ export default function App() {
       <Stack.Screen name="Tabs" component={Tabs} />
        <Stack.Screen name="TipsList" component={TipsList} />
        <Stack.Screen name="NewTip" component={NewTip} />
+       <Stack.Screen name="NewEvent" component={NewEvent} />
+       <Stack.Screen name="EventsList" component={EventsList} />
+
      </Stack.Navigator>
    
     </NavigationContainer>
